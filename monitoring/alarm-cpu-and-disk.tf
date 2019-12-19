6cef75bd2db62605152ad44700b608cdb018b40e
Paul Crimes <paul.crimes@solirius.com>
2019-12-19 13:53:06 +0000

    added critical level alarm for testing

--- monitoring/alarm-cpu-and-disk.tf
+++ monitoring/alarm-cpu-and-disk.tf
@@ -1,6 +1,6 @@
 
 
-resource "aws_cloudwatch_metric_alarm" "spgw_mpx_cpu" {
+resource "aws_cloudwatch_metric_alarm" "spgw_mpx_cpu_alert" {
   alarm_name = "${local.short_environment_name}__spgw__mpx-cpu__alert"
 
   //  comparison_operator       = "GreaterThanOrEqualToThreshold"
@@ -42,8 +42,32 @@ EOF
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "spgw_iso_cpu" {
+resource "aws_cloudwatch_metric_alarm" "spgw_iso_cpu_alert" {
   alarm_name = "${local.short_environment_name}__spgw__iso-cpu__alert"
+
+  //  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = <<EOF
+spgw iso cpu is high!
+Sometimes POs take their systems offline (and out of hours), this would cause this scenario but would be an ok situation
+https://dsdmoj.atlassian.net/wiki/spaces/DAM/pages/1578893538/Monitoring+and+Alerting
+EOF
+  alarm_actions       = ["${aws_sns_topic.alarm_notification.arn}"]
+
+  dimensions {
+    AutoScalingGroupName = "${data.terraform_remote_state.ecs_iso.autoscale_name}" //TODO sync output variable name style with crc & mpx in the new year
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "spgw_iso_cpu_critical" {
+  alarm_name = "${local.short_environment_name}__spgw__iso-cpu__critical"
 
   //  comparison_operator       = "GreaterThanOrEqualToThreshold"
   comparison_operator = "LessThanOrEqualToThreshold"
