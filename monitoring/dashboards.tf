7bba2ad6ad29ad1e16e8881efa9bbc8325332ecb
Paul Crimes <paul.crimes@solirius.com>
2019-12-20 11:23:28 +0000

    added alert count dashboard

--- monitoring/dashboards.tf
+++ monitoring/dashboards.tf
@@ -7,6 +7,17 @@ data "template_file" "dashboard_loadbalancer_body" {
   }
 }
 
+
+data "template_file" "alert_count_body" {
+  template = "${file("dashboard_alert_count.json")}"
+
+  vars {
+    region           = "${var.region}"
+    hmpps_asset_name_prefix = "${local.hmpps_asset_name_prefix}"
+  }
+}
+
+
 data "template_file" "dashboard_activemq_1_body" {
   template = "${file("dashboard_activemq_broker_1.json")}"
 
@@ -35,10 +46,20 @@ data "template_file" "dashboard_spg_instance_body" {
   }
 }
 
+
+
+
+
 resource "aws_cloudwatch_dashboard" "LB_monitoring" {
   dashboard_name = "${local.spg_app_name}_lb_monitoring"
   dashboard_body = "${data.template_file.dashboard_loadbalancer_body.rendered}"
 }
+
+resource "aws_cloudwatch_dashboard" "alert_count" {
+  dashboard_name = "${local.spg_app_name}_alert_count"
+  dashboard_body = "${data.template_file.alert_count_body.rendered}"
+}
+
 
 
 resource "aws_cloudwatch_dashboard" "amq_monitoring_broker_1" {
@@ -51,7 +72,7 @@ resource "aws_cloudwatch_dashboard" "amq_monitoring_broker_2" {
   dashboard_body = "${data.template_file.dashboard_activemq_2_body.rendered}"
 }
 
-resource "aws_cloudwatch_dashboard" "spg_monitoring" {
+resource "aws_cloudwatch_dashboard" "spg_instance_monitoring" {
   dashboard_name = "${local.spg_app_name}_instance_monitoring"
   dashboard_body = "${data.template_file.dashboard_spg_instance_body.rendered}"
 }
